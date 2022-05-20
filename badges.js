1b11fc5bfc471ccdb0b1e9b0d5f6cce232b1a227
AngelMond <abraham_will@hotmail.com>
2022-05-20 12:40:11 -0500

    This file stores all the badges the user can add to the readme file

---
+++ badges.js
@@ -0,0 +1,40 @@
+
+const html = `<img src="https://img.shields.io/badge/-HTML-e34f26?logo=html5&logoColor=fff">`;
+const css = `<img src="https://img.shields.io/badge/-CSS-1572B6?logo=css3&logoColor=fff">`;
+const JavaScript = `<img src="https://img.shields.io/badge/-JavaScript-F7DF1E?logo=javascript&logoColor=fff">`;
+const bootstrap = `<img src="https://img.shields.io/badge/-Bootstrap-7952B3?logo=bootstrap&logoColor=fff">`;
+const tailwind = `<img src="https://img.shields.io/badge/-Tailwind-06B6D4?logo=tailwind&logoColor=fff">`;
+const sass = `<img src="https://img.shields.io/badge/-SASS-CC6699?logo=sass&logoColor=fff">`;
+const nodejs = `<img src="https://img.shields.io/badge/-Node.js-339933?logo=node.js&logoColor=fff">`;
+const vuejs = `<img src="https://img.shields.io/badge/-Vue.js-4FC08D?logo=vue.js&logoColor=fff">`;
+const react = `<img src="https://img.shields.io/badge/-React-61DAFB?logo=react&logoColor=fff">`;
+const angular = `<img src="https://img.shields.io/badge/-Angular-DD0031?logo=angular&logoColor=fff">`;
+const java = `<img src="https://img.shields.io/badge/-Java-007396?logo=java&logoColor=fff">`;
+const typeScript = `<img src="https://img.shields.io/badge/-TypeScript-3178C6?logo=typescript&logoColor=fff">`;
+const php = `<img src="https://img.shields.io/badge/-PHP-777BB4?logo=php&logoColor=fff">`;
+const phyton = `<img src="https://img.shields.io/badge/-Phyton-3776AB?logo=phyton&logoColor=fff">`;
+const github = `<img src="https://img.shields.io/badge/-GitHub-181717?logo=github&logoColor=fff">`;
+const git = `<img src="https://img.shields.io/badge/-Git-F05032?logo=git&logoColor=fff">`;
+const mongoDB = `<img src="https://img.shields.io/badge/-MongoDB-47A248?logo=mongodb&logoColor=fff">`;
+const mysql = `<img src="https://img.shields.io/badge/-MySQL-003B57?logo=mysql&logoColor=fff">`;
+
+module.exports = {
+    html,
+    css,
+    JavaScript,
+    bootstrap,
+    tailwind,
+    sass,
+    nodejs,
+    vuejs,
+    react,
+    angular,
+    java,
+    typeScript,
+    php,
+    phyton,
+    github,
+    git,
+    mongoDB,
+    mysql
+}
